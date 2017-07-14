38e49993d0f820f5878e03150166d51070d1a6cb
Cesar Rodas <rodascesar03@yahoo.com>
2017-07-14 17:27:02 -0400

    added express and fixed adjusted img size

--- public/js/index.js
+++ public/js/index.js
@@ -3,25 +3,25 @@
 $(document).ready(function(){
     var title = "";
     var article;
-    
+
     $('.random').click(function(){
       window.location.href = "http://en.wikipedia.org/wiki/Special:Random";
     });
-    
+
     $(document).keypress(function(e) {
       if(e.which == 13) {
         title = document.getElementById("searchbox").value;
         search();
       }
     });
-  
+
     // My button clicking
     $('.search').click(function(){
       title = document.getElementById("searchbox").value;
       search();
     });
-  
-  
+
+
     // My ajax call to Wikipedia API
     function search(){
     $.ajax({
@@ -46,11 +46,11 @@ $(document).ready(function(){
       for(var key in pages){
         console.log(pages);
         if(!pages[key].thumbnail || !pages[key].title
-          || !pages[key].extract 
-          || pages[key] === undefined 
+          || !pages[key].extract
+          || pages[key] === undefined
           || !pages[key].thumbnail.source){
           continue;
-        } 
+        }
 
         count++;
         if(count % 3 === 1 || count === 1){
@@ -60,10 +60,10 @@ $(document).ready(function(){
         result += "<a href='http://en.wikipedia.org/?curid="
           + pages[key].pageid +"' target='_blank'>" +
         "<div class='col-md-4 text-center answer'>" +
-        "<div class='thumbnail'>" +
+        "<div class='thumbnail wiki_img'>" +
         "<div class=' img-responsive'>" +
-        "<img src='"+ pages[key].thumbnail.source + 
-          "' height='300px' width='300px'>" +
+        "<img src='"+ pages[key].thumbnail.source +
+          "' max-height='300px' max-width='300px'>" +
         "</div>"
         + "<div class='caption'>" +
         "<h3>" + pages[key].title + "</h3>" +
@@ -72,7 +72,7 @@ $(document).ready(function(){
         "</div>" +
         "</div>" +
         "</a>";
-        
+
         if(count % 3 === 0){
           result += "</div></div>";
         }
